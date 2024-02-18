b86e37c108e74f5e77d24974817458c8342a9a39
Valentyna Levchenko <valya@MacBook-Pro-Valentyna.local>
2024-02-18 17:46:19 +0100

    fix the indexhtml, client

--- svenglish-vite/src/client.js
+++ svenglish-vite/src/client.js
@@ -3,6 +3,8 @@ import { createClient } from "@sanity/client";
 const client = createClient({
   projectId: "41nkbk8d",
   dataset: "production",
+  apiVersion: "2024-02-18",
+  token: "",
 });
 
 export default client;
--- svenglish-vite/studio/sanity.cli.js
+++ svenglish-vite/studio/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '41nkbk8d',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
